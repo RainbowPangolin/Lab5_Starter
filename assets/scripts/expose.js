bc847a633fdc9e72eab07316fc10e0dfcf746f74
Kaiwen Tsou <kktsou@ucsd.edu>
2021-10-20 16:20:56 -0700

    Finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,9 +1,11 @@
 // expose.js
 
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+
+  const jsConfetti = new JSConfetti()
+
   //map image and sound to current horn
   const selectHorn = document.querySelector('#horn-select');
   selectHorn.addEventListener('change', function () {
@@ -14,8 +16,15 @@ function init() {
   //Plays selected sound on click
   const playButton = document.querySelector('button');
   playButton.addEventListener('click', function () {
-    document.querySelector('audio').play();
-    //if party horn, add confetti TODO
+    if(selectHorn.value != "select"){
+      document.querySelector('audio').play();
+      //if party horn, add confetti 
+      if(selectHorn.value == "party-horn"){
+        jsConfetti.addConfetti({
+          confettiRadius: 6,
+        })
+      }
+    }
   })
 
   //on volume change
@@ -42,8 +51,22 @@ function hornSnd(newHorn) {
 
 //change horn volume
 function changeVolume(vol){
+  let volumeIconElement = document.getElementById("volume-controls").querySelector('img');
+  let curVolume = document.getElementById("volume");
   //change playback volume 
   document.querySelector('audio').volume = vol/100;
-  //change volume icon TODO
+  //change volume icon 
+  if(curVolume.value <= 0) {
+    volumeIconElement.src = "assets/icons/volume-level-0.svg";
+  }
+  else if(curVolume.value > 0 && curVolume.value < 33){
+    volumeIconElement.src = "assets/icons/volume-level-1.svg";
+  }
+  else if(curVolume.value >= 33 && curVolume.value < 67){
+    volumeIconElement.src = "assets/icons/volume-level-2.svg";
+  }
+  else if(curVolume.value >= 67 ){
+    volumeIconElement.src = "assets/icons/volume-level-3.svg";
+  }
   
 }
