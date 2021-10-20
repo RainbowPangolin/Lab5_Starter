30554d1d23e3ad75040b7acfbbdf47f07c016050
Kaiwen Tsou <kktsou@ucsd.edu>
2021-10-20 15:49:15 -0700

    Some basic functionality for expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,49 @@
 // expose.js
 
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //map image and sound to current horn
+  const selectHorn = document.querySelector('#horn-select');
+  selectHorn.addEventListener('change', function () {
+    hornImg(document.getElementById('horn-select').value);
+    hornSnd(document.getElementById('horn-select').value);
+  });
+
+  //Plays selected sound on click
+  const playButton = document.querySelector('button');
+  playButton.addEventListener('click', function () {
+    document.querySelector('audio').play();
+    //if party horn, add confetti TODO
+  })
+
+  //on volume change
+  const volumeSlider = document.querySelector('#volume');
+  volumeSlider.addEventListener( 'change', function (){
+    console.log(document.getElementById("volume").value);
+    changeVolume(document.getElementById("volume").value);
+  })
+
+}
+
+//change horn image
+function hornImg(newHorn) {
+  let image = document.querySelector('img');
+  image.src = "assets/images/" + newHorn + ".svg";
+}
+
+//change horn sound file
+function hornSnd(newHorn) {
+  console.log(newHorn);
+  let sound = document.querySelector('audio');
+  sound.src = "assets/audio/" + newHorn + ".mp3";
+}
+
+//change horn volume
+function changeVolume(vol){
+  //change playback volume 
+  document.querySelector('audio').volume = vol/100;
+  //change volume icon TODO
+  
 }
